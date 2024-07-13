9623e1829b4656299dd3ac2fddd1323159c7451a
Vineet Jindal <vineet.jindal.21cse@bmu.edu.in>
2024-07-13 12:03:49 +0530

    first commit

---
+++ 1-react-app/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  console.log('app');
+  return (
+    <>
+      <h1>hello its vineet here</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 1-react-app/src/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Header() {
+  console.log("header");
+  return (
+    <>
+      <h2>This is header file</h2>
+    </>
+  );
+}
+
+export default Header;
